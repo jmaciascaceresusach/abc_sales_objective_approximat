ff7e8448f46ccdea06f4178bf4369ad20aa3d96f
jmaciascaceresusach <131014656+jmaciascaceresusach@users.noreply.github.com>
2024-05-06 18:21:46 -0400

    UPD: Visualización de los parámetros y sus pbb en el resultado.

--- src/SimulationEngine.cpp
+++ src/SimulationEngine.cpp
@@ -62,6 +62,7 @@ void SimulationEngine::runSimulations(int numberOfIterations,
         std::cout << "\n";
         std::cout << "***Result***\n";
         std::cout << "Optimal parameters found within tolerance.\n";
+        td::cout << "Parameter: " << param.name << ", Probability: " << param.probability << std::endl;
         std::cout << "\n";
     } else {
         std::cout << "\n";
