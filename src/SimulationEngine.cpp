7d10dd62e03d81e6607a628e775f532889fb47f2
jmaciascaceresusach <131014656+jmaciascaceresusach@users.noreply.github.com>
2024-05-06 19:33:54 -0400

    UPD: Últimos comentarios SimulationEngine.cpp

--- src/SimulationEngine.cpp
+++ src/SimulationEngine.cpp
@@ -78,24 +78,6 @@ void SimulationEngine::runSimulations(int numberOfIterations,
             std::cout << "\n";
         }
     }
-
-    /*if (bestOutcome != outcomes.end() && std::abs(bestOutcome->saleValue - salesObjective) <= tolerance) {
-        // Actualizar parámetros con el mejor resultado encontrado
-        this->parameters = bestOutcome->parameters;
-        std::cout << "\n";
-        std::cout << "***Refined Parameters - Result***\n";
-        std::cout << "Optimal parameters found within tolerance.\n";
-        std::cout << "Best saleValue: " << bestOutcome->saleValue << std::endl;  // Mostrar el mejor saleValue
-        for (auto& param : this->parameters) {
-            std::cout << "Parameter: " << param.name << ", Probability: " << param.probability << std::endl;
-        }
-        std::cout << "\n";
-    } else {
-        std::cout << "\n";
-        std::cout << "***Result***\n";
-        std::cout << "No optimal parameters were found within tolerance.\n";
-        std::cout << "\n";
-    }*/
 }
 
 void SimulationEngine::adjustParameters(double saleValue, double salesObjective) {
