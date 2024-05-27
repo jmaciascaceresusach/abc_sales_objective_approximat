f50b79d4ad30f7ac859ca77041c5be460c016cc4
Joaquín Macías Cáceres <joaquin.macias@usach.cl>
2024-05-27 19:25:14 -0400

    UDP: Actualización de fix de SimulationEngine.cpp

--- src/SimulationEngine.cpp
+++ src/SimulationEngine.cpp
@@ -1,26 +1,8 @@
 #include "../include/SimulationEngine.h"
-#include <thread>
-#include <mutex>
-#include <vector>
-#include <string>
-#include <functional>
 #include <iostream>
-#include <fstream>
-#include <iomanip>
+#include <algorithm> // para std::min_element
 #include <chrono>
-#include <ctime>
-#include <sstream> // Incluir sstream para convertir std::thread::id a string
-
-void logFunction(const std::string& message) {
-    std::ofstream logFile("simulation_log.txt", std::ios_base::app);
-    if (logFile.is_open()) {
-        auto now = std::chrono::system_clock::now();
-        std::time_t now_time = std::chrono::system_clock::to_time_t(now);
-        std::tm now_tm = *std::localtime(&now_time);
-        logFile << std::put_time(&now_tm, "%Y-%m-%d %H:%M:%S") << " - " << message << "\n";
-        logFile.close();
-    }
-}
+#include <iomanip> // para std::put_time
 
 /**
  * Implementación del constructor.
@@ -47,6 +29,7 @@ void SimulationEngine::runSimulations(int numberOfIterations, std::function<doub
     std::time_t now_time = std::chrono::system_clock::to_time_t(now);
     std::tm now_tm = *std::localtime(&now_time);
 
+    // Abrir archivo para guardar estadísticas de simulación
     std::ofstream statsFile("statistics_simulations.txt");
     statsFile << "Creation Date and Time: " << std::put_time(&now_tm, "%Y-%m-%d %H:%M:%S") << "\n";
     statsFile << "Iteration,SaleValue,Distance,Objective,Tolerance";
@@ -55,143 +38,47 @@ void SimulationEngine::runSimulations(int numberOfIterations, std::function<doub
     }
     statsFile << "\n";
 
-    std::mutex mtx;
-    std::vector<Parameter> bestParameters;
     double closestDistance = std::numeric_limits<double>::max();
-    std::string bestMethod;
+    std::vector<Parameter> bestParameters = parameters;
+    double bestSaleValue = 0;
     int bestIteration = -1;
 
     std::cout << "\n**Start Simulation***\n";
-    logFunction("**Start Simulation***");
 
-    auto runAdjustmentSimple = [&, this](void (ABCMethod::*adjustFunc)(std::vector<Parameter>&, double, double), const std::string& methodName) {
-        std::vector<Parameter> localParameters = parameters;
-        double localClosestDistance = std::numeric_limits<double>::max();
-        std::vector<Parameter> localBestParameters = localParameters;
-        double localBestSaleValue = 0;
-        int localBestIteration = -1;
+    // Ejecutar iteraciones de simulación
+    for (int i = 0; i < numberOfIterations; ++i) {
+        double saleValue = calculateSale(parameters);
+        double distance = std::abs(saleValue - salesObjective);
 
-        for (int i = 0; i < numberOfIterations; ++i) {
-            double saleValue = calculateSale(localParameters);
-            double distance = std::abs(saleValue - salesObjective);
+        std::cout << "\n";
+        std::cout << "Iteration: " << i << " - saleValue: " << saleValue << " - distance: " << distance << std::endl;
 
-            std::stringstream ss;
-            ss << "\n";
-            ss << "Thread " << std::this_thread::get_id() << " - Iteration: " << i << " - saleValue: " << saleValue << " - distance: " << distance;
-            std::string logMsg = ss.str();
-            std::cout << logMsg << std::endl;
-            logFunction(logMsg);
+        statsFile << i << "," << saleValue << "," << distance << "," << salesObjective << "," << tolerance;
+        for (const auto& param : parameters) {
+            statsFile << "," << param.probability;
+        }
+        statsFile << "\n";
 
-            {
-                std::lock_guard<std::mutex> lock(mtx);
-                statsFile << i << "," << saleValue << "," << distance << "," << salesObjective << "," << tolerance;
-                for (const auto& param : localParameters) {
-                    statsFile << "," << param.probability;
-                }
-                statsFile << "\n";
-            }
-
-            if (distance < localClosestDistance) {
-                localClosestDistance = distance;
-                localBestParameters = localParameters;
-                localBestSaleValue = saleValue;
-                localBestIteration = i;
-            }
-
-            (abcMethod.*adjustFunc)(localParameters, saleValue, salesObjective);
+        if (distance < closestDistance) {
+            closestDistance = distance;
+            bestParameters = parameters;
+            bestSaleValue = saleValue;
+            bestIteration = i;
         }
 
-        {
-            std::lock_guard<std::mutex> lock(mtx);
-            if (localClosestDistance < closestDistance) {
-                closestDistance = localClosestDistance;
-                bestParameters = localBestParameters;
-                bestMethod = methodName;
-                bestIteration = localBestIteration;
-            }
-        }
-    };
-
-    /*auto runAdjustmentComplex = [&, this](void (ABCMethod::*adjustFunc)(std::vector<Parameter>&, std::function<double(const std::vector<Parameter>&)>, double, double), const std::string& methodName) {
-        std::vector<Parameter> localParameters = parameters;
-        double localClosestDistance = std::numeric_limits<double>::max();
-        std::vector<Parameter> localBestParameters = localParameters;
-        double localBestSaleValue = 0;
-        int localBestIteration = -1;
-
-        for (int i = 0; i < numberOfIterations; ++i) {
-            double saleValue = calculateSale(localParameters);
-            double distance = std::abs(saleValue - salesObjective);
-
-            std::stringstream ss;
-            ss << "\n";
-            ss << "Thread " << std::this_thread::get_id() << " - Iteration: " << i << " - saleValue: " << saleValue << " - distance: " << distance;
-
-            std::string logMsg = ss.str();
-            std::cout << logMsg << std::endl;
-            logFunction(logMsg);
-
-            {
-                std::lock_guard<std::mutex> lock(mtx);
-                statsFile << i << "," << saleValue << "," << distance << "," << salesObjective << "," << tolerance;
-                for (const auto& param : localParameters) {
-                    statsFile << "," << param.probability;
-                }
-                statsFile << "\n";
-            }
-
-            if (distance < localClosestDistance) {
-                localClosestDistance = distance;
-                localBestParameters = localParameters;
-                localBestSaleValue = saleValue;
-                localBestIteration = i;
-            }
-
-            (abcMethod.*adjustFunc)(localParameters, calculateSale, saleValue, salesObjective);
-        }
-
-        {
-            std::lock_guard<std::mutex> lock(mtx);
-            if (localClosestDistance < closestDistance) {
-                closestDistance = localClosestDistance;
-                bestParameters = localBestParameters;
-                bestMethod = methodName;
-                bestIteration = localBestIteration;
-            }
-        }
-    };*/
-
-    /*std::vector<std::thread> threads;
-    threads.emplace_back([&] { runAdjustmentSimple(&ABCMethod::dynamicAdjustParameters, "dynamicAdjustParameters"); });
-    threads.emplace_back([&] { runAdjustmentSimple(&ABCMethod::dynamicAdjustParametersGradient, "dynamicAdjustParametersGradient"); });
-    threads.emplace_back([&] { runAdjustmentSimple(&ABCMethod::dynamicAdjustParametersSlidingAverage, "dynamicAdjustParametersSlidingAverage"); });
-    threads.emplace_back([&] { runAdjustmentComplex(&ABCMethod::dynamicAdjustParametersGenetic, "dynamicAdjustParametersGenetic"); });
-    threads.emplace_back([&] { runAdjustmentComplex(&ABCMethod::dynamicAdjustParametersSimulatedAnnealing, "dynamicAdjustParametersSimulatedAnnealing"); });
-    threads.emplace_back([&] { runAdjustmentSimple(&ABCMethod::dynamicAdjustParametersLM, "dynamicAdjustParametersLM"); });*/
-
-    std::vector<std::thread> threads;
-    threads.emplace_back([&] { runAdjustmentSimple(&ABCMethod::dynamicAdjustParameters, "dynamicAdjustParameters"); });
-
-    for (auto& thread : threads) {
-        thread.join();
+        adjustParameters(saleValue, salesObjective);
     }
 
     parameters = bestParameters;
     statsFile.close();
 
     std::cout << "\n***End Simulation***\n";
-    logFunction("***End Simulation***");
 
-    std::string resultMsg = "Best parameters found with sale value " + std::to_string(calculateSale(bestParameters)) + " using method: " + bestMethod + " in iteration " + std::to_string(bestIteration);
     std::cout << "\n***Results***\n";
-    std::cout << resultMsg << std::endl;
-    logFunction(resultMsg);
-
+    std::cout << "Best parameters found at iteration " << bestIteration << " with sale value " << bestSaleValue << std::endl;
     std::cout << "\n***Best Parameters***\n";
     for (const auto& param : bestParameters) {
-        std::string paramMsg = "Parameter: " + param.name + ", Probability: " + std::to_string(param.probability);
-        std::cout << paramMsg << std::endl;
-        logFunction(paramMsg);
+        std::cout << "Parameter: " << param.name << ", Probability: " << param.probability << std::endl;
     }
 
     abcMethod.refineParameters(parameters, calculateSale, salesObjective, tolerance);
@@ -204,21 +91,13 @@ void SimulationEngine::runSimulations(int numberOfIterations, std::function<doub
  */
 void SimulationEngine::adjustParameters(double saleValue, double salesObjective) {
     double error = saleValue - salesObjective;
-    double learningRate = 0.001;
+    double learningRate = 0.01;
 
     for (auto& param : parameters) {
         double adjustment = (error > 0 ? -1 : 1) * learningRate * std::abs(param.probability);
         param.adjustProbability(adjustment);
 
-        // Evitar que los parámetros se ajusten a cero
-        if (param.probability < 0.01) {
-            param.probability = 0.01;
-        } else {
-            param.probability = std::max(0.0, std::min(param.probability, 1.0));
-        }
-
-        std::string paramMsg = "Parameter: " + param.name + ", Probability: " + std::to_string(param.probability);
-        std::cout << paramMsg << std::endl;
-        logFunction(paramMsg);
+        param.probability = std::max(0.0, std::min(param.probability, 1.0));
+        std::cout << "Parameter: " << param.name << ", Probability: " << param.probability << std::endl;
     }
 }
