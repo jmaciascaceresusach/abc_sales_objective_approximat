d1d56b7ef98c9106103ccebf629fe5388c757019
Joaquín Macías Cáceres <joaquin.macias@usach.cl>
2024-07-21 20:29:13 -0400

    UDP: - Fixed para tomar el valor de un int.

--- src/main.cpp
+++ src/main.cpp
@@ -14,7 +14,7 @@
 void readConfigFor3(const std::string& configFilePath, 
                     int& numberOfIterations,
                     double& tolerance,
-                    int& daysToSimulate) {
+                    double& daysToSimulate) {
     std::ifstream configFile(configFilePath);
     std::string line;
 
@@ -27,7 +27,7 @@ void readConfigFor3(const std::string& configFilePath,
                 if (getline(iss, value)) {
                     if (key == "numberOfIterations") numberOfIterations = std::stoi(value);
                     else if (key == "tolerance") tolerance = std::stod(value);
-                    else if (key == "daysToSimulate") daysToSimulate = std::stoi(value);
+                    else if (key == "daysToSimulate") daysToSimulate = std::stod(value);
                 }
             }
         }
@@ -40,8 +40,8 @@ int main(int argc, char* argv[]) {
     // Obtiene el tiempo de inicio
     auto start = std::chrono::high_resolution_clock::now();
 
-    int numberOfIterations, daysToSimulate;
-    double tolerance;
+    int numberOfIterations;
+    double tolerance, daysToSimulate;
 
     readConfigFor3("../data/simulation_config.txt", 
                    numberOfIterations, 
